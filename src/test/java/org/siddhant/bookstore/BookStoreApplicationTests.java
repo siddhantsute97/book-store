3d36fb12f7949e9bb6ffaf8670266e4272748db5
siddhant97 <siddhantsute@gmail.com>
2023-03-25 15:27:35 +0530

    This project is about Book Store

---
+++ src/test/java/org/siddhant/bookstore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.siddhant.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
